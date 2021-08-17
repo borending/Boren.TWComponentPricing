4bfa5d3a04bf76b8fe1487fa0551a398112861af
Boren <kai21413@gmail.com>
2021-08-17 15:27:27 +0800

    get options

--- Boren.TWComponentPricing.Worker/Service/CrawlerService.cs
+++ Boren.TWComponentPricing.Worker/Service/CrawlerService.cs
@@ -14,9 +14,9 @@ namespace Boren.TWComponentPricing.Worker.Service
         private async Task<(Browser Browser, Page Page)> GetWebAsync()
         {
             await new BrowserFetcher().DownloadAsync();
-            var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = false });
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
             var page = (await browser.PagesAsync()).First();
-            await page.SetViewportAsync(new ViewPortOptions { Width = 1024, Height = 768 });
+            //await page.SetViewportAsync(new ViewPortOptions { Width = 1024, Height = 768 });
             await page.GoToAsync(COOLPC_URL);
             await page.WaitForTimeoutAsync(3000);
 
@@ -25,18 +25,29 @@ namespace Boren.TWComponentPricing.Worker.Service
 
         public async Task<IList<object>> GetAsync()
         {
+            // DOM 操作
             var web = await this.GetWebAsync();
-            await web.Page.WaitForTimeoutAsync(3000);
+            await web.Page.WaitForTimeoutAsync(1000);
 
-            var html = await web.Page.GetContentAsync();
-            var document = new HtmlDocument();
-            document.LoadHtml(html);
-
-            var trs = document.DocumentNode.SelectNodes("#tbdy tr");
-            foreach(var tr in trs)
+            // 商品類別 #tbdy tr
+            var categories = await web.Page.QuerySelectorAllAsync("#tbdy tr");
+            foreach (var category in categories)
             {
-                var title = tr.ChildNodes.SingleOrDefault(c => c.HasClass("t"));
-                //var title = (await tr.("td.t"));
+                // 類別名稱 tr.t
+                var categoryName = await category.QuerySelectorAsync(".t").EvaluateFunctionAsync<string>("e => e.innerHTML");
+                // 商品 td select.s optgroup option
+                var items = await category.QuerySelectorAllAsync("td select.s optgroup option");
+
+                // 2021.08.17 
+                // todo: 把列表轉換成物件
+                // todo: 資料庫結構規劃
+                var itemNames = new List<string>();
+                foreach (var item in items)
+                {
+                    var text = await item.EvaluateFunctionAsync<string>("e => e.innerHTML");
+                    var clss = await (await item.GetPropertyAsync("className")).JsonValueAsync<string>();
+                    itemNames.Add(text);
+                }
             }
 
             // table id : Tfix
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 電腦零組件歷史價格 TWComponentPricing
 ## 參考資料
 PuppeteerSharp Github： https://github.com/hardkoded/puppeteer-sharp
-PuppeteerSharp 使用範例： https://blog.darkthread.net/blog/puppeteer-sharp/
+PuppeteerSharp 使用範例： https://gist.github.com/vmandic/70da11954cb2b654b6ff899ac73bed0c
 
