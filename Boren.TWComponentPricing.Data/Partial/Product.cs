6587cefc8060b6040815a4c35d0965dcbb3a82bf
Boren <kai21413@gmail.com>
2022-06-20 18:03:11 +0800

    資料需要清洗

---
+++ Boren.TWComponentPricing.Data/Partial/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Boren.TWComponentPricing.Data
+{
+    public partial class Product
+    {
+        public string GetFixedText()
+        {
+            string fixedText = Regex.Replace(this.OriginText, @"【.*?】", " ");
+            fixedText = Regex.Replace(fixedText, @"\(.*?\)", "");
+            fixedText = Regex.Replace(fixedText, @"(?:[\w\.]+\/)+[\w\.]+", "");
+
+            var trims = new List<string> { ",", "贈", "+" };
+            trims.ForEach(target =>
+            {
+                if (fixedText.Contains(target))
+                    fixedText = fixedText.Substring(0, fixedText.LastIndexOf(target));
+            });
+
+            return fixedText.Trim();
+        }
+    }
+}
--- Boren.TWComponentPricing.Tests/CrawlerTests.cs
+++ Boren.TWComponentPricing.Tests/CrawlerTests.cs
@@ -1,7 +1,11 @@
-﻿using Boren.TWComponentPricing.Worker;
+﻿using Boren.TWComponentPricing.Data;
+using Boren.TWComponentPricing.Worker;
 using Boren.TWComponentPricing.Worker.Service;
 using Newtonsoft.Json;
 using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Boren.TWComponentPricing.Tests
@@ -16,7 +20,6 @@ namespace Boren.TWComponentPricing.Tests
             _crawlerService = new CrawlerService();
         }
 
-
         [Test]
         public async Task GetAsyncTest()
         {
@@ -29,6 +32,19 @@ namespace Boren.TWComponentPricing.Tests
         {
             var list = await _crawlerService.GetAsync();
             await _crawlerService.SetAsync(list);
+        }
+
+        [Test]
+        public void GetFixedTextTest()
+        {
+            var builder = new StringBuilder();
+
+            var dbContext = new PricingDbContext();
+            var entities = dbContext.Products.Select(x => x).ToList();
+            foreach (var entity in entities)
+                builder.AppendLine(entity.GetFixedText());
+
+            Console.Write(builder.ToString());
         }
     }
 }
